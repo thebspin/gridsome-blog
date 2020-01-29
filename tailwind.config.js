a5467df78be14e1027dacc8c348b89ac21927818
Dennis <dennis@webconcern.nl>
2020-01-29 09:54:28 +0100

    Added bases styles for headings

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,13 @@ module.exports = {
     extend: {},
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    function({ addBase, config }) {
+      addBase({
+        'h1': { fontSize: config('theme.fontSize.3xl') },
+        'h2': { fontSize: config('theme.fontSize.xl') },
+        'h3': { fontSize: config('theme.fontSize.lg') },
+      })
+    }
+  ],
 }
